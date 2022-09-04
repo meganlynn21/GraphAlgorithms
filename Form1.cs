a5fdf14c8a543f56990b718d4b14c2da36869bae
meganlynn21 <meganlkeyes0203@gmail.com>
2022-09-04 04:19:27 -0400

    reading in the data from the transportation website with JSON and deserializing it, converting to json array and adding objects from that into the listbox

--- Form1.cs
+++ Form1.cs
@@ -21,8 +21,6 @@ namespace GraphAlgorithms
 {
     public partial class Form1 : Form
     {
-        
-
         public Form1()
         {
             InitializeComponent();
@@ -30,31 +28,21 @@ namespace GraphAlgorithms
 
         private void lstBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-
         }
-            private void AllFlightsBtn_Click(object sender, EventArgs e)
-            {
-                WebClient client = new WebClient(); // needs using System.Net;
-                string url = "https://data.transportation.gov/resource/4f3n-jbg2.json?year=2021";
-                string webPage = client.DownloadString(url);
-            //var parser = JArray.Parse(webPage);
-            //Form1 convert = JsonConvert.DeserializeObject<Form1>(webPage);
-            //lstBox.Items.Add(convert);
-            //var parser = JArray.Parse(webPage["members"].ToString());
-            List<JArray> converting = new List<JArray>();
+
+        private void AllFlightsBtn_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient(); // needs using System.Net;
+            string url = "https://data.transportation.gov/resource/4f3n-jbg2.json?year=2021";
+            string webPage = client.DownloadString(url);
             webPage.Replace("[", "").Replace("]", "");
             var deserialize = (JArray)JsonConvert.DeserializeObject(webPage, typeof(JArray));
-       
+
             foreach (JObject obj in deserialize)
             {
 
-                lstBox.Items.Add(obj);
+                lstBox.Items.Add("" + obj);
             }
- 
-
-
-
         }
 
     }
